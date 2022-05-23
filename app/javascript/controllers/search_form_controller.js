e9af629caad84de02f328501848d6cd94458343b
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-23 22:49:35 +0200

    Fixed linked to show article

--- app/javascript/controllers/search_form_controller.js
+++ app/javascript/controllers/search_form_controller.js
@@ -3,6 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="search-form"
 export default class extends Controller {
   search() {
-    console.log('chiki  ')
+    console.log(this.element)
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(()=>{
+        this.element.requestSubmit();
+      }, 200)
   }
 }

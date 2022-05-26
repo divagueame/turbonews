ed402b83b4b70bfca885e12938276644085e2591
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-26 17:44:48 +0200

    Add Tags Helper

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -1,2 +1,14 @@
 module TagsHelper
+
+  def show_active_state(tag)
+    content = ''
+    if controller_name == 'tags' && action_name == 'show'
+        html = "
+        <strong>Active:</strong>
+        #{tag.active} <p class='font-bold mr-3 text-md'>"
+      content << html
+    end
+    content.html_safe
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'admin', to: 'admin#index'
   resources :tags
   resources :sources
   resources :articles

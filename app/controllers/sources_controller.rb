70300968232c204999384dbc7d234d92eb7b2c79
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-26 10:32:17 +0200

    Moved scraped to a function + Add Scrap Report

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,9 +1,8 @@
-
 require 'nokogiri'
 require 'open-uri'
 
 class SourcesController < ApplicationController
-  before_action :set_source, only: %i[ show edit update destroy ]
+  before_action :set_source, only: %i[show edit update destroy]
 
   # GET /sources or /sources.json
   def index
@@ -11,35 +10,24 @@ class SourcesController < ApplicationController
   end
 
   def scrap
+    reports = []
     Source.all.each do |source|
-      id = source.id
-      url = source.url
-      selector = source.selector
-      relative_url = source.relative_url
-
-      html = URI.open(source.url.to_s).read
-      header = Nokogiri::HTML(html)
-      header.css(selector).each do |piece|
-        @article = Article.new
-        @article.header = piece.text
-        @article.url = piece.attribute('href').value
-        @article.source_id = source.id
-
-          if(@article.valid?)
-            @article.save
-            p "Article saved"
-          else
-            p 'Article not saved'
-          end
-        end
+      scrap_report = scrap_source(source)
+      reports.push(scrap_report)
     end
+    
+    # Reports of the scraping
+    reports.each do |report| 
+      puts "\n"
+      pp "Source: #{report[:source_name]}"
+      pp "Saved: #{report[:saved]}"
+      pp "Not saved: #{report[:not_saved]}"
+    end
+  
+    puts "\n"
   end
-   
 
- 
-  # GET /sources/1 or /sources/1.json
-  def show
-  end
+  def show; end
 
   # GET /sources/new
   def new
@@ -47,8 +35,7 @@ class SourcesController < ApplicationController
   end
 
   # GET /sources/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /sources or /sources.json
   def create
@@ -56,7 +43,7 @@ class SourcesController < ApplicationController
 
     respond_to do |format|
       if @source.save
-        format.html { redirect_to source_url(@source), notice: "Source was successfully created." }
+        format.html { redirect_to source_url(@source), notice: 'Source was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -67,7 +54,7 @@ class SourcesController < ApplicationController
   def update
     respond_to do |format|
       if @source.update(source_params)
-        format.html { redirect_to source_url(@source), notice: "Source was successfully updated." }
+        format.html { redirect_to source_url(@source), notice: 'Source was successfully updated.' }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -79,18 +66,45 @@ class SourcesController < ApplicationController
     @source.destroy
 
     respond_to do |format|
-      format.html { redirect_to sources_url, notice: "Source was successfully destroyed." }
+      format.html { redirect_to sources_url, notice: 'Source was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_source
-      @source = Source.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def source_params
-      params.require(:source).permit(:name, :url, :selector, :relative_url)
+  def scrap_source(source)
+    report = {:source_name=> source.name, :not_saved => 0, :saved => 0}
+ 
+    id = source.id
+    url = source.url
+    selector = source.selector
+    relative_url = source.relative_url
+
+    html = URI.open(source.url.to_s).read
+    header = Nokogiri::HTML(html)
+    header.css(selector).each do |piece|
+      @article = Article.new
+      @article.header = piece.text
+      @article.url = piece.attribute('href').value
+      @article.source_id = source.id
+
+      if @article.valid?
+        @article.save
+        report[:saved] += 1  
+      else
+        report[:not_saved] += 1  
+      end
     end
+    
+    report
+  end
+
+  def set_source
+    @source = Source.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def source_params
+    params.require(:source).permit(:name, :url, :selector, :relative_url)
+  end
 end

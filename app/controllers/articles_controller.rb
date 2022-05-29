28fedda9fe30b998976d8ce2531c101457608eee
Divagueame <martin.arce.teixeira@gmail.com>
2022-05-29 19:06:34 +0200

    Add articles scraper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,17 @@
 require 'nokogiri'
 require 'open-uri'
 class ArticlesController < ApplicationController
-  before_action :set_article, only: %i[ show edit update destroy scrape_article]
+  before_action :set_article, only: %i[show edit update destroy scrape_article]
 
   # GET /articles or /articles.json
   def index
-    if params[:query].present?
+    @articles = if params[:query].present?
       # @articles = Article.where("header LIKE ?", "#{params[:query]}%")
-      @articles = Article.search_all("#{params[:query]}");
-      
-    else
-      @articles = Article.all
-    end
+      Article.search_all(params[:query].to_s)
+
+                else
+                  Article.all
+                end
 
     if turbo_frame_request?
       render partial: 'articles', locals: { articles: @articles }
@@ -21,8 +21,7 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1 or /articles/1.json
-  def show
-  end
+  def show; end
 
   # GET /articles/new
   def new
@@ -30,19 +29,17 @@ class ArticlesController < ApplicationController
   end
 
   # GET /articles/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /articles or /articles.json
   def create
-    
     @article = Source.first.articles.new(article_params)
 
     respond_to do |format|
       if @article.save
-        format.html { redirect_to article_url(@article), notice: "Article was successfully created." } 
+        format.html { redirect_to article_url(@article), notice: 'Article was successfully created.' }
       else
-        format.html { render :new, status: :unprocessable_entity } 
+        format.html { render :new, status: :unprocessable_entity }
       end
     end
   end
@@ -51,9 +48,9 @@ class ArticlesController < ApplicationController
   def update
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." } 
+        format.html { redirect_to article_url(@article), notice: 'Article was successfully updated.' }
       else
-        format.html { render :edit, status: :unprocessable_entity } 
+        format.html { render :edit, status: :unprocessable_entity }
       end
     end
   end
@@ -63,64 +60,63 @@ class ArticlesController < ApplicationController
     @article.destroy
 
     respond_to do |format|
-      format.html { redirect_to articles_url, notice: "Article was successfully destroyed." }
+      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   def scrape_article
-    # article_id = params[:id].to_i
-    # @article = Article.find(params[:id])
     get_article_url(@article)
-    if(@article.browsed)
+    if @article.browsed
       p 'Already browsed. Skip'
     else
       p 'Never browser'
       article_url = get_article_url(@article)
 
       html = URI.open(article_url).read
-      retrieved_data = Nokogiri::HTML(html)
-      p 'retrieved_data: '
-      p retrieved_data
+      html = Nokogiri::HTML.parse(html)
+      retrieved_title = html.title
+      retrieved_paragraphs = html.css('article p')
+      retrieved_paragraphs = retrieved_paragraphs.map { |paragraph| paragraph.text }
+
+      total_words_count = 0
+      retrieved_paragraphs.each { |piece| total_words_count += piece.split.size }
+
+      puts 'Total Words Count'
+      p total_words_count
+      p article_url
       puts "\n"
-      puts "\n"
-      puts "\n"
-      
+      puts retrieved_paragraphs
+
     end
-    
-    puts "\n"
-    puts "\n"
   end
 
   private
-    def scrape_body(html)
 
-    end
+  def scrape_body(html); end
 
-    # Use callbacks to share common setup or constraints between actions.
-    def set_article
-      @article = Article.find(params[:id])
-    end
+  def set_article
+    @article = Article.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def article_params
-      params.require(:article).permit(:header, :body, :url, :source_id)
-    end
+  def article_params
+    params.require(:article).permit(:header, :body, :url, :source_id)
+  end
 
-    def valid_url?(url)
-      (url[0,3] === 'htt') || (url[0,3] === 'www')? true : false
-    end
+  def valid_url?(url)
+    (url[0, 3] === 'htt') || (url[0, 3] === 'www') ? true : false
+  end
 
-    def get_article_url(article)
-      if valid_url?(article.url)
-        article.url
-      elsif(valid_url?(article.source.url + article.url))
-        article.source.url + article.url
-      else 
-        p 'ERROR. Not available url'
-        p article.source.url
-        p article.url
-        nil
-      end
+  def get_article_url(article)
+    if valid_url?(article.url)
+      article.url
+    elsif valid_url?(article.source.url + article.url)
+      article.source.url + article.url
+    else
+      p 'ERROR. Not available url'
+      p article.source.url
+      p article.url
+      nil
     end
+  end
 end

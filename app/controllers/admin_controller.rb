7eef4fa9ced993266312be832ed6ee873445cd30
Divagueame <martin.arce.teixeira@gmail.com>
2022-06-22 21:11:05 +0200

    Add tests for admin controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,10 +2,8 @@ class AdminController < ApplicationController
   def index
   end
 
-  def find_tags
-    p 'Find tags'
-    redirect_to root_path
-  end
+  
+
   private
 
 end
--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,8 +1,29 @@
 require "test_helper"
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  setup do
     get admin_url
+  end
+
+  test "should get index" do
     assert_response :success
+    
+    assert_select "h1", text: "Admin Panel"
+  end
+  
+  test 'be see sources' do
+    assert_select ".source-card", 2
+  end
+
+  test 'be able to scan one sources' do
+    
+  end
+
+  test 'should able to scan all sources' do
+    
+  end
+
+  test 'should see todays scraped articles' do
+    
   end
 end
